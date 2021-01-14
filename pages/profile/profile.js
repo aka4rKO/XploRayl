ff7e52ef33db15c90a136301f96aa270e920cdf6
MerianLesster <merianlesster27.ml@gmail.com>
2021-01-14 18:57:33 +0530

    addded jquery-circle-progress

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,1 +1,15 @@
-$("#tab-title").html = "Achievements";
+$(document).ready(function () {
+    $('#circle').circleProgress({
+        value: 0.6,
+        size: 190,
+        lineCap: "round",
+        thickness: 20,
+        startAngle: 2,
+        animationStartValue: 5 - 5,
+        fill: {
+          gradient: ["cyan", "#04d9d9"]
+        }
+ }).on('circle-animation-progress', function(event, progress) {
+        $(this).find('strong').html(Math.round(100 * progress) + '<i>%</i>');
+      });
+  });
