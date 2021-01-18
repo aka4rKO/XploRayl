2528fef8c81e4f5c2fe034dc72fdfb4f0ed3ce79
Azhar <azharanees1@gmail.com>
2021-01-18 11:22:06 +0530

    my-hunt final page

---
+++ pages/my-hunts/my-hunts.js
@@ -0,0 +1,83 @@
+function myMap() {
+    var mapProp = {
+      center: new google.maps.LatLng(51.508742, -0.12085),
+      zoom: 5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  }
+  
+  function initMap() {
+    var poiId = JSON.parse(sessionStorage.getItem("currentId"));
+  
+    var dataObj = JSON.parse(localStorage.getItem("data"));
+    var pois = dataObj["poiObjs"];
+  
+    var poi = pois.filter((obj) => {
+      return obj.poiId === poiId;
+    });
+  
+    var poi = poi[0];
+  
+    var latitude = poi["lat"];
+    var longitude = poi["lng"];
+  
+    const myLatLng = { lat: latitude, lng: longitude };
+    const map = new google.maps.Map(document.getElementById("map-container"), {
+      zoom: 12,
+      center: myLatLng,
+    });
+    new google.maps.Marker({
+      position: myLatLng,
+      map,
+      title: "Hello World!",
+    });
+  }
+
+
+
+$(document).ready(function(){
+    $('.rating').rating();
+  });
+      
+  
+  $(document).ready(function(){
+      $("#review-btn" ).click(function() {
+           var reviewMessage = $("#review-message").val();
+           var ratings = $("#ratingSlider").val()
+  
+           var postReqData = {
+               id : 12,
+               reviewMessage : reviewMessage,
+               ratings:ratings
+           }
+  
+           var ratingsList = JSON.parse(localStorage.getItem("reviews"))
+           var ratingsArray = [] ;
+  
+           if(ratingsList){
+               ratingsArray = ratingsList;
+           }
+           ratingsArray.push(postReqData)
+           localStorage.setItem("reviews", JSON.stringify(ratingsArray));
+           console.log(ratingsArray);
+           window.location.href = "../profile/profile.html"
+
+  
+          });
+
+    });
+
+
+  $(document).ready(function(){
+      $("#end-hunt" ).click(function() {
+           
+  
+          });
+    });
+  
+  
+  
+    
+  
+  
+  
