1151b2bf6cb36661ec6e4a272420001e1ddcff5d
aka4rKO <akram.arm.7@gmail.com>
2021-01-20 20:41:20 +0530

    booking flow

--- pages/booking/booking.js
+++ pages/booking/booking.js
@@ -1,22 +1,91 @@
-$(function() {
-    $( "#datepicker" ).datepicker();
-  } );
+$(document).ready(function () {
+  var currentId = sessionStorage.getItem("currentId");
+  var dataObj = JSON.parse(localStorage.getItem("data"));
+  var pois = dataObj["poiObjs"];
 
-$(function() {
-  $("#book-now")
-    .on("click", function(){
-      window.location.href='order.html';
+  var poi = pois.filter((obj) => {
+    return obj.poiId == currentId;
+  });
 
-    $(".train").addClass('train-animation');
-  })
-    .on("animationend", function(){
-    $(".train").removeClass('train-animation');
+  var passengers = sessionStorage.getItem("passengers");
+
+  $("#arrival").append(poi[0].arrival);
+  $("#treasureMap").append(`£ ${poi[0].price}`);
+  $("#trainTicketPrice").append(`£ ${2 * passengers}`);
+
+  sessionStorage.setItem("arrival", poi[0].arrival);
+  sessionStorage.setItem("treasureMapAmt", poi[0].price);
+
+  var tot = Number(poi[0].price) + Number(2 * passengers);
+  sessionStorage.setItem("total", tot);
+
+  $("#subTotal").append(`£ ${tot}`);
+  $("#total").append(`£ ${tot}`);
+
+  renderCoupons();
+});
+
+function renderCoupons() {
+  var couponsRendered = "";
+
+  var userObj = JSON.parse(localStorage.getItem("user"));
+  var coupons = userObj["coupons"];
+
+  coupons.forEach((element) => {
+    couponsRendered += `
+    <a href="#" rel="modal:close" onclick="applyCoupon('${element.name}', '${element.code}', '${element.amount}', '${element.expiryDate}');">
+      <div class="coupon-card">
+        <img
+          class="coupon-img"
+          src="./../../assets/images/coin.png"
+          alt="coupon"
+        />
+        <h2 class="coupon-name">${element.name}</h2>
+        <p class="coupon-validity">Valid till ${element.expiryDate}</p>
+        <p class="coupon-code">${element.code}</p>
+        <h2 class="coupon-price">£ ${element.amount}</h2>
+      </div>
+    </a>
+    `;
+  });
+
+  $("#coupons").append(couponsRendered);
+}
+
+function applyCoupon(name, code, amount, expiryDate) {
+  console.log(name);
+  $("#couponName").append(name);
+  $("#couponAmount").append(`£ ${amount}`);
+
+  var tot = sessionStorage.getItem("total");
+  $("#subTotal").empty().append(`£ ${tot}`);
+  $("#total").empty().append(`£ ${tot - amount}`);
+}
+
+$(function () {
+  $("#datepicker").datepicker();
+});
+
+$(function () {
+  $("#book-now").on("click", function () {
+    var dept = $("#dept").val();
+    var date = $("#datepicker").val();
+    var passengers = $("#passengers").val();
+
+    sessionStorage.setItem("departure", dept);
+    sessionStorage.setItem("date", date);
+    sessionStorage.setItem("passengers", passengers);
+
+    $(".train").addClass("train-animation");
+
+    setTimeout(function () {
+      window.location.href = "order.html";
+    }, 1200);
   });
 });
 
-$(function() {
-  $("#payment-button")
-    .on("click", function(){
-      window.location.href='./../payment/add-card.html';
-  })
+$(function () {
+  $("#payment-button").on("click", function () {
+    window.location.href = "./../payment/add-card.html";
+  });
 });
--- pages/payment/add-card.js
+++ pages/payment/add-card.js
@@ -1,14 +1,14 @@
-
 $(document).ready(function () {
+  $(".ui-input-text").css("border", "unset");
+
   loadCardList();
-}
-);
+});
 
 function loadCardList() {
   var cardsComp = "";
-  var cardList = JSON.parse(localStorage.getItem("cardList"))
+  var cardList = JSON.parse(localStorage.getItem("cardList"));
   if (cardList) {
-    cardList.forEach(card => {
+    cardList.forEach((card) => {
       cardsComp += `
     <div class="order-field center-row placeholder-text">
       <div class="add-card-placeholder label-text">
@@ -24,113 +24,100 @@ function loadCardList() {
   }
   $("#card-list").append(cardsComp);
 }
-$(document).ready(function () {
-  $("#save-card")
-    .on("click", function () {
 
-      var cardName = $("#cardName").val();
-      var cardNumber = $("#cardNumber").val();
-      var expiryDate = $("#expiryDate").val();
+function saveCard() {
+  var cardName = $("#cardName").val();
+  var cardNumber = $("#cardNumber").val();
+  var expiryDate = $("#expiryDate").val();
 
-      var expiryArray = expiryDate.split('/');
-      var cvv = $("#cvc").val();
-      $.ajax({
-        type: 'POST',
-        url: 'https://api.stripe.com/v1/tokens',
-        headers: {
-          Authorization: 'Bearer sk_test_51HMs5AIDK6dRU6bAiHjaW1hU95a9VHqSWXzChedCrYm2Hb2I0vaotvmOQ8l1YgCy9vWbBQJ1ZvlSjz8w41MO25M500IZSSkvaC'
-        },
-        "data": {
-          "card[number]": `${cardNumber}`,
-          "card[exp_month]": `${Number(expiryArray[0])}`,
-          "card[exp_year]": `${Number(expiryArray[1])}`,
-          "card[cvc]": `${cvv}`,
-          "card[name]": `${cardName}`
-        },
-        success: function (response) {
-          console.log('Card Added Successfully: ', response);
+  var expiryArray = expiryDate.split("/");
+  var cvv = $("#cvc").val();
+  $.ajax({
+    type: "POST",
+    url: "https://api.stripe.com/v1/tokens",
+    headers: {
+      Authorization:
+        "Bearer sk_test_51HMs5AIDK6dRU6bAiHjaW1hU95a9VHqSWXzChedCrYm2Hb2I0vaotvmOQ8l1YgCy9vWbBQJ1ZvlSjz8w41MO25M500IZSSkvaC",
+    },
+    data: {
+      "card[number]": `${cardNumber}`,
+      "card[exp_month]": `${Number(expiryArray[0])}`,
+      "card[exp_year]": `${Number(expiryArray[1])}`,
+      "card[cvc]": `${cvv}`,
+      "card[name]": `${cardName}`,
+    },
+    success: function (response) {
+      console.log("Card Added Successfully: ", response);
 
+      var postReqData = {
+        tokenId: response.id,
+        id: response.card.id,
+        exp_month: response.card.exp_month,
+        exp_year: response.card.exp_year,
+        last4: response.card.last4,
+        name: cardName,
+      };
 
-          var postReqData = {
-            tokenId: response.id,
-            id: response.card.id,
-            exp_month: response.card.exp_month,
-            exp_year: response.card.exp_year,
-            last4: response.card.last4,
-            name: cardName
-          }
-
-          var cardList = [];
-          if (localStorage.getItem("cardList")) {
-            var cardList = JSON.parse(localStorage.getItem("cardList"))
-          }
-          var cardsArray = [];
-
-          if (cardList) {
-            cardsArray = cardList;
-          }
-          cardsArray.push(postReqData)
-          localStorage.setItem("cardList", JSON.stringify(cardsArray));
-          console.log(cardsArray);
-
-        },
-        error: (response) => {
-          console.log('error payment: ', response.responseJSON.error.message);
-          $(".cardErr").html(response.responseJSON.error.message);
-        }
-      })
-    })
-});
-$(document).ready(function () {
-  $("#book-now")
-    .on("click", function () {
-      cardList = [];
+      var cardList = [];
       if (localStorage.getItem("cardList")) {
-        cardList = localStorage.getItem("cardList");
-
+        var cardList = JSON.parse(localStorage.getItem("cardList"));
       }
-      var card = JSON.parse(cardList)[cardList.length - 1]
+      var cardsArray = [];
 
-      $.ajax({
-        type: 'POST',
-        url: 'https://api.stripe.com/v1/charges',
-        headers: {
-          Authorization: 'Bearer sk_test_51HMs5AIDK6dRU6bAiHjaW1hU95a9VHqSWXzChedCrYm2Hb2I0vaotvmOQ8l1YgCy9vWbBQJ1ZvlSjz8w41MO25M500IZSSkvaC'
-        },
-        "data": {
-          "amount": 2000,
-          "currency": "usd",
-          "source": "tok_1IAlAEIDK6dRU6bAwLiVhYBT"
-        },
-        success: function (response) {
-          console.log('Successfully Purchased: ', response);
-
-        },
-        error: (response) => {
-          console.log('error payment: ', response.responseJSON.error.message);
-          $(".cardErr").html(response.responseJSON.error.message);
-        }
-      })
-      location.href = "./success.html"
-
-    })
-});
-
+      if (cardList) {
+        cardsArray = cardList;
+      }
+      cardsArray.push(postReqData);
+      localStorage.setItem("cardList", JSON.stringify(cardsArray));
+      console.log(cardsArray);
+    },
+    error: (response) => {
+      console.log("error payment: ", response.responseJSON.error.message);
+      $(".cardErr").html(response.responseJSON.error.message);
+    },
+  });
+  loadCardList();
+}
 
 $(document).ready(function () {
-  $("#success-ok-btn")
-    .on("click", function () {
+  $("#book-now").on("click", function () {
+    cardList = [];
+    if (localStorage.getItem("cardList")) {
+      cardList = localStorage.getItem("cardList");
+    }
+    var card = JSON.parse(cardList)[cardList.length - 1];
 
-      location.href = "./confirmation.html"
+    $.ajax({
+      type: "POST",
+      url: "https://api.stripe.com/v1/charges",
+      headers: {
+        Authorization:
+          "Bearer sk_test_51HMs5AIDK6dRU6bAiHjaW1hU95a9VHqSWXzChedCrYm2Hb2I0vaotvmOQ8l1YgCy9vWbBQJ1ZvlSjz8w41MO25M500IZSSkvaC",
+      },
+      data: {
+        amount: 2000,
+        currency: "usd",
+        source: "tok_1IAlAEIDK6dRU6bAwLiVhYBT",
+      },
+      success: function (response) {
+        console.log("Successfully Purchased: ", response);
+      },
+      error: (response) => {
+        console.log("error payment: ", response.responseJSON.error.message);
+        $(".cardErr").html(response.responseJSON.error.message);
+      },
+    });
+    location.href = "./success.html";
+  });
+});
 
-    })
+$(document).ready(function () {
+  $("#success-ok-btn").on("click", function () {
+    location.href = "./confirmation.html";
+  });
 });
 $(document).ready(function () {
-  $("#goto-hunt")
-    .on("click", function () {
-
-      location.href = "./../my-hunts/my-hunts.html"
-
-
-    })
+  $("#goto-hunt").on("click", function () {
+    location.href = "./../my-hunts/my-hunts.html";
+  });
 });
